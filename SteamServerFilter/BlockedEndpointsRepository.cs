98199b1b863f7123725c74d5627a16598786f7e9
deximy <deximy@gmail.com>
2023-02-23 12:08:25 +0800

    feat: add a repository to store blocked endpoints

---
+++ SteamServerFilter/BlockedEndpointsRepository.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+
+namespace SteamServerFilter
+{
+    public class BlockedEndpointsRepository
+    {
+        private readonly List<IPEndPoint> blocked_endpoints_list_;
+
+        public BlockedEndpointsRepository()
+        {
+            blocked_endpoints_list_ = new List<IPEndPoint>();
+        }
+
+        public IReadOnlyCollection<IPEndPoint> Get()
+        {
+            return blocked_endpoints_list_;
+        }
+
+        public void Add(IPEndPoint endpoint)
+        {
+            if (!blocked_endpoints_list_.Contains(endpoint))
+            {
+                blocked_endpoints_list_.Add(endpoint);
+            }
+        }
+
+        public void Add(IPAddress address, ushort port)
+        {
+            Add(new(address, port));
+        }
+    }
+}
