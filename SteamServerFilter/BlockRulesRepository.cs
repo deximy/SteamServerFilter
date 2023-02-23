98d2754fabad52e59d4d66a4a3e3ee474bef2ffb
deximy <deximy@gmail.com>
2023-02-23 11:54:28 +0800

    fix & refactor: duplicate detection doesn't work
    Cause:
    
    `Equal` method or `==` operator doesn't compare the two patterns but `Regex` objects.
    
    Fix:
    
    `Regex.ToString` method will return the pattern. Comparing the patterns instead of the objects solves this problem.
    
    Also, to avoid unnecessary conversion, do duplicate detection on raw pattern. The method that accepts a `Regex` object as an argument is useless, so it's removed.

--- SteamServerFilter/BlockRulesRepository.cs
+++ SteamServerFilter/BlockRulesRepository.cs
@@ -16,17 +16,12 @@ namespace SteamServerFilter
             return block_rules_list_;
         }
 
-        public void Add(Regex rule)
-        {
-            if (!block_rules_list_.Contains(rule))
-            {
-                block_rules_list_.Add(rule);
-            }
-        }
-
         public void Add(string rule)
         {
-            Add(new Regex(rule));
+            if (!block_rules_list_.Any(i => i.ToString() == rule))
+            {
+                block_rules_list_.Add(new Regex(rule));
+            }
         }
     }
 }
