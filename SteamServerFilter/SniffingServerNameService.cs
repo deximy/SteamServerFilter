142c0398ff12854e0851fe3f0045bb0419b1812f
deximy <deximy@gmail.com>
2023-02-24 17:21:49 +0800

    feat: add active sniffing service
    This service can query endpoints actively and get their response. If the response contain names matching the rules, `IncomingServerNameFilterService` will handle this endpoint.

---
+++ SteamServerFilter/SniffingServerNameService.cs
@@ -0,0 +1,54 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace SteamServerFilter
+{
+    public class SniffingServerNameService
+    {
+        private readonly UdpClient sniffing_socket_;
+        private readonly byte[] a2s_query_data_;
+        private readonly byte[] a2s_query_challange_prefix_;
+
+        public SniffingServerNameService()
+        {
+            sniffing_socket_ = new UdpClient(new IPEndPoint(IPAddress.Any, 0));
+            LogService.Debug($"Local sniffing socket port: {((IPEndPoint?)sniffing_socket_.Client.LocalEndPoint)?.Port}");
+
+            a2s_query_data_ = new byte[] {
+                0xFF, 0xFF, 0xFF, 0xFF,
+                0x54, 0x53, 0x6F, 0x75,
+                0x72, 0x63, 0x65, 0x20,
+                0x45, 0x6E, 0x67, 0x69,
+                0x6E, 0x65, 0x20, 0x51,
+                0x75, 0x65, 0x72, 0x79,
+                0x00
+            };
+            a2s_query_challange_prefix_ = new byte[] { 0xFF, 0xFF, 0xFF, 0xFF, 0x41 };
+
+            Task.Run(
+                async () => {
+
+                    while (true)
+                    {
+                        var response = await sniffing_socket_.ReceiveAsync();
+                        if (response.Buffer.Length == 9 && response.Buffer.SequenceEqual(a2s_query_challange_prefix_))
+                        {
+                            var challange = new ArraySegment<byte>(response.Buffer, response.Buffer.Length - 4, 4);
+                            await sniffing_socket_.SendAsync(a2s_query_data_.Concat(challange).ToArray(), response.RemoteEndPoint);
+                        }
+                    }
+                }
+            );
+        }
+
+        public int GetSocketPort()
+        {
+            return ((IPEndPoint?)sniffing_socket_.Client.LocalEndPoint)?.Port ?? 0;
+        }
+
+        public async Task<int> QueryServerName(IPEndPoint endpoint)
+        {
+            return await sniffing_socket_.SendAsync(a2s_query_data_, endpoint);
+        }
+    }
+}
